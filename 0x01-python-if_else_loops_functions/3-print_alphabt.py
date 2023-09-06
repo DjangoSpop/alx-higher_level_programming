c34b07e817740e3cdd660d9b8d5e7dc94692f0d0
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2023-09-07 00:50:56 +0300

    we can

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for char_code in range(ord('a'), ord('z') + 1):
-    if chr(char_code) != 'q' and chr(char_code) != 'e':
-        print(chr(char_code), end='')
 
-# Output:
-# abcdfghijklmnoprstuvwxyz
+for i in range(97, 123):
+    if i == 113 or i == 101:
+        continue
+    print("{:c}".format(i), end="")
