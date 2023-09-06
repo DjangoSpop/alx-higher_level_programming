8d857e3e9b686f7550567281a90ade0364795ac1
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2023-09-07 00:46:11 +0300

    we can

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for num in range(100):
-    if num < 99:
-        print(f"{02d}, ", end='')
+
+for i in range(100):
+    if i < 99:
+        print("{:02d}".format(i), end=", ")
     else:
-        print(f"{02d}")
+        print("{:d}".format(i))
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,10 @@
-for i in range(10):
-    for j in range(i+1, 10):
-        print(f"{i}{j:02d}, ", end='')
+#!/usr/bin/python3
 
-# Print the last number followed by a new line
-print("99")
+for i in range(0, 10):
+    for j in range(1, 10):
+        if i >= j:
+            continue
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j))
+        else:
+            print("{:d}{:d}".format(i, j), end=", ")
