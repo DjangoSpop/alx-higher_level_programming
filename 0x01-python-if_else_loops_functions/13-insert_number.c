0ba88bf79451daacaa9cf72e187c631ed3647226
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2023-09-07 01:04:56 +0300

    we can

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,13 +1,35 @@
-#!/usr/bin/python3
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * insert_node - inserts a number into a sorted singly linked list.
+ * @head: pointer to the pointer of the list.
+ * @number: number to be inserted.
+ *
+ * Return: address f the new node or NULL if it failed.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *before, *new_Node;
 
-
-def fizzbuzz():
-    for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=" ")
-        elif i % 5 == 0:
-            print("Buzz", end=" ")
-        elif i % 3 == 0:
-            print("Fizz", end=" ")
-        else:
-            print("{:d}".format(i), end=" ")
+	new_Node = malloc(sizeof(listint_t));
+	if (new_Node != NULL)
+	{
+		new_Node->n = number;
+		if (*head == NULL || (*head)->n >= new_Node->n)
+		{
+			new_Node->next = *head;
+			*head = new_Node;
+			return (new_Node);
+		}
+		else
+		{
+			before = *head;
+			while (before->next != NULL && before->next->n < new_Node->n)
+				before = before->next;
+			new_Node->next = before->next;
+			before->next = new_Node;
+			return (new_Node);
+		}
+	}
+	return (NULL);
+}
