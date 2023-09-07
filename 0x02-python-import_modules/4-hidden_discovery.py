2e398f5b36d9e30a5350c92f392ece071c73025e
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2023-09-08 01:26:30 +0300

    we can do this

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
     import hidden_4
 
     names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
+    sorted_names = sorted(names)
+
+    for name in sorted_names:
+        if not name.startswith("__"):
             print(name)
