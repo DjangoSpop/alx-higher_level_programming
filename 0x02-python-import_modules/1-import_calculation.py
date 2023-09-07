93b5695026e6fc583443230167b828cf238b5c26
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2023-09-08 00:43:37 +0300

    we can do it achive dreams

--- 0x02-python-import_modules/1-import_calculation.py
+++ 0x02-python-import_modules/1-import_calculation.py
@@ -1,8 +1,8 @@
-from calculator import add, sub, mul, div
+from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
 result1 = add(a, b)
-result2 = subtract(a, b)
-result3 = multiply(a, b)
+result2 = sub(a, b)
+result3 = mul(a, b)
 
 print ("{result1}/n , {result2}/n , {result3}")
