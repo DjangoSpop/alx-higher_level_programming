7d97a6c45fc37ce42e0ade35be1ae1da00efd5d8
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2023-09-08 00:56:22 +0300

    we can code

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,9 +1,3 @@
-#!/bin/python3
+#!/usr/bin/python3
 import string
-import sys
-
-def main():
-    sys.stdout.write(string.ascii_uppercase + '\n')
-
-if __name__ == "__main__":
-    main()
+print(string.ascii_uppercase)
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-from sys import argv
-
-def main():
-    num_args = len(argv) - 1
-    print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}:")
-
-    for i in range(num_args):
-        print(f"{i + 1}: {argv[i + 1]}")
 
 if __name__ == "__main__":
-    main()
+    """Print the number of and list of arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
