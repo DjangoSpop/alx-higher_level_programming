eb950bfc10035a3341a4cfc75dffae487778a2c9
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2023-09-08 01:29:37 +0300

    i hate codeig

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
 
-def magic_calculation(a, b):
-    """Match bytecode provided"""
-    from magic_calculation_102 import add, sub
+if __name__ == "__main__":
+    # Check for correct number of arguments
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return (c)
+    # Assign variables for clarity
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
 
+    # Check the operator and perform the appropriate calculation
+    if operator == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif operator == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif operator == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif operator == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
     else:
-        return (sub(a, b))
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
