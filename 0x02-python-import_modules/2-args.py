af20d7216eb5ff594eba90aa351bfaa6d92d0f73
DjangoSpop <behodjango@gmail.com>
2023-09-08 11:18:15 +0300

    we do dream kill

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 
-if __name__ == '__main__':
-    import sys
+if __name__ == "__main__":
+    from sys import argv
+    i = 1
+    args = len(argv) - 1
 
-    arg_count = len(sys.argv) - 1
-
-    if arg_count == 0:
-        print("0 arguments.")
-    elif arg_count == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(arg_count))
-
-    for i in range(1, arg_count + 1):
-        print("{}: {}".format(i, sys.argv[i]))
+    print(f"{args} {'argument' if args == 1 else 'arguments'}", end="")
+    print(f"{'.' if args == 0 else ':'}")
+    while (i <= args):
+        print(f"{i}: {argv[i]}")
+        i += 1
