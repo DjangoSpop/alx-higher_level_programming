49c5f8834f20fa01848d54ef757148b109f2db92
DjangoSpop <behodjango@gmail.com>
2023-09-08 11:12:56 +0300

    we can do this

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-# Define variables a and b
+
 a = 1
 b = 2
 
-# Import the add function from add_0.py
+
 from add_0 import add
 
-# Calculate the result of add(a, b)
-result = add(a, b)
+result = (a, b)
 
-# Print the result with string formatting
 print(f"{a} + {b} = {result}")
 
