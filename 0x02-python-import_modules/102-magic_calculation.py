fe50074b0f422f0e18089fb55530fa72afbb1f50
DjangoSpop <behodjango@gmail.com>
2023-09-09 23:15:26 +0300

    we can do this

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,8 @@
-
+#!/usr/bin/python3
 def magic_calculation(a, b):
-    add, sub = __import__('magic_calculation_102', globals(), locals(), ['add', 'sub'], 0)
     if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return c
+        add = __import__('magic_calculation_102').add
+        sub = __import__('magic_calculation_102').sub
+        return add(a, b)
     else:
         return sub(a, b)
