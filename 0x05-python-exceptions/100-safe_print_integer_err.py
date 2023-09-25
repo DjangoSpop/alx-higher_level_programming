033a0acaaa876e5fe92315f8ffc0ce96c8924c79
DjangoSpop <behodjango@gmail.com>
2023-09-26 01:11:04 +0300

    we can learn until we try or cry

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,19 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import sys
+
 
 def safe_print_integer_err(value):
     try:
-        integer_value = int(value)
-        print("{:d}".format(integer_value))
-        return True
-    except ValueError:
-        # If the conversion to int fails, print the error message to stderr
-        print("Exception: Cannot print as integer", file=sys.stderr)
-        return False
-
-# Example usage:
-result1 = safe_print_integer_err(42)
-print("Result 1:", result1)  # Should print True
-
-result2 = safe_print_integer_err("Hello")
-print("Result 2:", result2)  # Should print False and print an error message to stderr
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
