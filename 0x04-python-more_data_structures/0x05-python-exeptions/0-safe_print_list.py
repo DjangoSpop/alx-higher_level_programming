b86b3bb741affa86ec2702ab74f332db343f6550
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2023-09-26 20:40:40 +0300

    we can do this

--- 0x04-python-more_data_structures/0x05-python-exeptions/0-safe_print_list.py
+++ 0x04-python-more_data_structures/0x05-python-exeptions/0-safe_print_list.py
@@ -1,18 +1,13 @@
+#!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    count = 0  # Initialize a count to keep track of printed elements
-    
-    try:
-        for i in range(x):
-            print(my_list[i], end='')  # Print the element without a newline
-            count += 1  # Increment the count of printed elements
-    except IndexError:
-        pass  # Catch the IndexError if it occurs (list index out of range)
-
-    print()  # Print a newline to separate the output
-    return count  # Return the count of printed elements
-
-# Example usage:
-my_list = [1, 2, 3, 4, 5]
-x = 3
-printed_elements = safe_print_list(my_list, x)
-print("Number of elements printed:", printed_elements)
+  
+    hey = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            hey += 1
+        except IndexError:
+            break
+    print("")
+    return (hey)
