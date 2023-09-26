38c4c81647d5104799d3e4c072524e421ac6255e
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2023-09-26 20:40:40 +0300

    we can do this

--- 0x05-python-exceptions/5-raise_exception.py
+++
@@ -1,8 +0,0 @@
-def raise_exception():
-    raise TypeError("This is a custom type exception")
-
-# Example usage:
-try:
-    raise_exception()
-except TypeError as e:
-    print("Caught an exception:", e)
--- 0x05-python-exeptions/6-raise_exception_msg.py
+++
@@ -1,8 +0,0 @@
-def raise_exception_msg(message=""):
-    raise NameError(message)
-
-# Example usage:
-try:
-    raise_exception_msg("This is a custom name exception")
-except NameError as e:
-    print("Caught an exception:", e)
