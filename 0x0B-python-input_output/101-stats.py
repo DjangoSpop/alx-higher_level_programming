6d575065c281276c92a5eab6a60cf4336787e9f5
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2023-10-10 14:59:07 +0300

    we can do anything

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,7 +2,6 @@
 """Reads from standard input and computes metrics
 """
 
-
 def print_stats(size, status_codes):
     """Print accumulated metrics
     """
@@ -10,13 +9,12 @@ def print_stats(size, status_codes):
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
 
-
 if __name__ == "__main__":
     import sys
 
     size = 0
     status_codes = {}
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    valid_codes = {'200', '301', '400', '401', '403', '404', '405', '500'}
     count = 0
 
     try:
@@ -27,20 +25,15 @@ if __name__ == "__main__":
             else:
                 count += 1
 
-            line = line.split()
-
+            line = line.strip().split('\t')  # Assuming tab-separated input
+            
             try:
-                size += int(line[-1])
+                if len(line) >= 3:
+                    size += int(line[-1])
+                    
+                    if line[-2] in valid_codes:
+                        status_codes[line[-2]] = status_codes.get(line[-2], 0) + 1
             except (IndexError, ValueError):
-                pass
-
-            try:
-                if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1:
-                        status_codes[line[-2]] = 1
-                    else:
-                        status_codes[line[-2]] += 1
-            except IndexError:
                 pass
 
         print_stats(size, status_codes)
