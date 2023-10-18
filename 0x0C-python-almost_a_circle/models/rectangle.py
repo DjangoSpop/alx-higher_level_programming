4a1c79433b5424f685262f9dbc05d084b19c6d61
DjangoBeho <behodjango@gmail.com>
2023-10-18 12:49:27 +0300

    we cam

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -55,5 +55,42 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+    
+    def area(self):
+        if __name__ = "__main__":
+            r1 = Rectangle(3, 2)
+            print(r1.area())
+            
+            r2 = Rectangle(2, 10)
+            print(r2.area())
+            
+            r3 = Rectangle(8, 7, 0, 0, 12)
+            print(r3.area())
+            
+    def display(self):
+        if __name__ = "__main__":
+            r1 = Rectangle(4, 6)
+            r1.display()
+            
+            r2 = Rectangle(2, 2)
+            r2.display()
+"""Update the class Rectangle by overriding the __str__ method so that it returns [Rectangle] (<id>) <x>/<y> - <width>/<height>
 
-        
+You can overwrite it but dont forget to call the parent class with super() since we implemented previously a logic inside this method."""
+
+def __str__(self):
+    return f"[Rectangle] ({id(self)}) {self.x}/{self.y} - {self.width}/{self.height}"
+    
+    # def update(self, *args):
+    #     if args:
+    #         arg_count = len(args)
+    #         if arg_count >= 1:
+    #             self.id = args[0]
+    #         if arg_count >= 2:
+    #             self.width = args[1]
+    #         if arg_count >= 3:
+    #             self.height = args[2]
+    #         if arg_count >= 4:
+    #             self.x = args[3]
+    #         if arg_count >= 5:
+    #             self.y = args[4]
