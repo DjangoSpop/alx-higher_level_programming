64cbd367a5a441d13592f0b2e357f66b15d2d5d3
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2023-12-13 17:47:10 +0200

    we can

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,2 +1,14 @@
-//write theat computer factorial
-// the first argument can be cast 
+// Write the computer factorial
+// The first argument can be cast 
+function factorial(n) {
+    if (isNaN(n)) {
+        return 1;
+    }
+    if (n === 0) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+console.log(factorial(parseInt(process.argv[2])));
+
---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,5 @@
+const obj = {
+    value: 12
+};
+
+obj.value = 89;
