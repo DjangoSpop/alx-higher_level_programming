30edc3529f0b6476640e0e1f8b1cfdeb54a8f5b9
DjangoBeho <behodjango@gmail.com>
2023-12-14 03:28:17 +0200

    we can

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+//write a class that defines a regtangle
+
+class Rectangle{
+  constructor(w,h){
+    this.width = w;
+    this.height = h;
+  }
+    
+}
---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,10 @@
+class Rectangle {
+    constructor(w, h) {
+        if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+            // Create an empty object
+            return {};
+        }
+        this.width = w;
+        this.height = h;
+    }
+}
