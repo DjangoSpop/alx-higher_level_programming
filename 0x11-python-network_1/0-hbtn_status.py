e0bbb46f7890ed75a624778ad723f7cbe6f8b724
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-01-07 12:07:02 +0200

    python

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
+"""This script is used to fetch and display the tabulated output from a specified URL.
+
+
+
+Requirements:
+ This script requires Python 3 and the urllib module.
+
+Environment Variables:
+ You can specify the URL to fetch the data from using the "URL" environment variable.
+ If no environment variable is specified, the default URL "https://alx-intranet.hbtn.io/status" will be used.
+
+Notes:
+ The script utilizes a with statement to ensure that the response is properly closed after reading.
+ It also correctly checks if each line starts with a '-' before printing it. This ensures that only the tabulated lines are printed, as required.
+
+ You can use the "requests" module to simplify the code and improve readability, but this module is not included in the standard Python library.
+
+"""
 import urllib.request
 
 url = "https://alx-intranet.hbtn.io/status"
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-import urlib.request, urlib.parse
+import urllib, urllib.request
 import sys
-
+"""
+we are her takeing to arguments and posting it to the website decodeing
+the email from asci to utf 8 
+ Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)
+"""
 
 url = sys.argv[1]
 email = {'email': sys.argv[2]}
 
 
 
-data = urlib.parse.urlencode(email)
+data = urllib.parse.urlencode(email)
 data = data.encode('ascii')
 
 with urllib.request.urlopen(url, data) as response:
-    print(response.read().decode('utf-8))
+    print(response.read().decode('utf-8'))
